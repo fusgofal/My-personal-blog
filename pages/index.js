8e5e36115f6d904ed286acd1773e66e6f1479411
Santiago Muñoz <smunoz@tecmovin.com>
2020-11-27 22:38:19 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import 'tailwindcss/tailwind.css'
+
+const HomePage = () => (
+  <>
+    <h1 className="text-gray-800 text-center text-3xl mt-20 mb-5">Welcome to Santiagoo.com</h1>
+    <p className="text-center text-gray-600">Albert Santiago Muñoz Castaño's blog</p>
+  </>
+);
+
+export default HomePage
